fb3d62b728dc2ff78f4eb5182b1ce7076d0dd850
LeonelMer <111746138+LeonelMer@users.noreply.github.com>
2023-09-25 15:22:34 -0300

    Update Producto.js

--- src/pages/Producto.js
+++ src/pages/Producto.js
@@ -10,12 +10,15 @@ export default function Producto() {
   if (producto) {
     return (
       <div className="product-details">
-        <img src={producto.image} alt={producto.name} className="product-image" />
+        <br></br>
+        <img src={producto.image} alt={producto.name} className="product-imageDetalles" />
         <div className="product-info">
           <h1 className="product-name">{`Nombre: ${producto.name}`}</h1>
           <p className="product-description">{`Descripción: ${producto.descripcion}`}</p>
           <p className="product-category">{`Categoría: ${producto.categoria}`}</p>
         </div>
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
       </div>
     );
   } else {
